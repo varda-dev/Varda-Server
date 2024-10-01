5c68d8975d4bee6187bcce480b4a993225e67366
varda-dev <varda-dev@protonmail.com>
2024-09-30 21:24:18 -0400

    quick test

--- setup-minecraft.sh
+++ setup-minecraft.sh
@@ -20,7 +20,11 @@ fi
 if [ ! -d "$VARDA_DIR" ]; then
     sudo mkdir -p /srv/minecraft/varda
     sudo unzip "$VARDA_ZIP" -d "$VARDA_DIR"
-    sudo cp start.sh "$VARDA_DIR"
+    #sudo cp start.sh "$VARDA_DIR"
+    sudo tee "$VARDA_DIR"/start.sh &>/dev/null <<EOF
+    #!/usr/bin/env sh
+    /usr/bin/java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.10/unix_args.txt "$@" nogui
+    EOF
     echo "eula=true" | sudo tee -a "$VARDA_DIR"/eula.txt
     sudo chown -R minecraft:minecraft /srv/minecraft/varda
     (cd "$VARDA_DIR" && sudo -u minecraft -H sh -c "java -jar forge-*-installer.jar --installServer")
